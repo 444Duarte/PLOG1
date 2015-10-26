77a53b077d5153806ba6f33e503c1dd07155e849
unknown <up201304777@B03AV1403.salas.feupsig.fe.up.pt>
2015-10-26 18:40:43 +0000

    Criar board dinamicamente.

--- game.pl
+++ game.pl
@@ -1,7 +1,7 @@
 % Initialize the board
 initBoard([
 [ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
+[ [0|0], [1|2], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
 [ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
 [ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
 [ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
@@ -17,33 +17,58 @@ initBoard([
 ).
 
 % Print triangles
-printTriangle([0|_]) :- write('|  ').
+printTriangle([0|_]) :- write('|   ').
 printTriangle([1|1]) :- 	write('| '),
 							name(C,[9651]), 
-							write(C).
+							write(C),
+							write(' ').
 printTriangle([1|2]) :- 	write('| '),
 							name(C,[9650]), 
-							write(C).
+							write(C),
+							write(' ').
 printTriangle([2|1]) :- 	write('| '),
 							name(C,[9661]), 
-							write(C).
+							write(C),
+							write(' ').
 printTriangle([2|2]) :- 	write('| '),
 							name(C,[9660]), 
-							write(C).							
+							write(C),
+							write(' ').							
 
 % Print utils
-printBoarder:- write('----------------------------------------\n').
+printBoarder([]):- write('-\n').
+printBoarder([_| NEXT_COLUMNS]):- 		write('----'),
+										printBoarder(NEXT_COLUMNS).
 printRow([]):- write('|\n').
 printRow([TRIANGLE | NEXT_TRIANGLES]) :-
 										printTriangle(TRIANGLE),
 										printRow(NEXT_TRIANGLES).
 
 % Print the board
-printBoard([]) :- printBoarder.
-printBoard([ROW | NEXT_ROWS]) :-  
-								printBoarder,
+printBoard([ROW|[]]) :-	printBoarder(ROW),
+							printRow(ROW),
+							printBoarder(ROW). % Last row
+printBoard([ROW | NEXT_ROWS]) :-printBoarder(ROW),
 								printRow(ROW),
 								printBoard(NEXT_ROWS).
 
+								
+createBoard(BOARD, COLUMNS, ROWS):- 
+									createRow(BOARD, COLUMNS, ROWS).
+									
+createRow([X|[]], COLS, 1) :- createColumn(X,COLS).
+createRow(X, COLS, 0):- createColumn(X,COLS).						
+createRow( [X|Y],COLS,ROWS):- 
+							createRow(X, COLS, 0),
+							N is ROWS-1,
+							createRow(Y, COLS, N).
+
+createColumn([[0|0]] ,1).
+createColumn([0|0], 0).						
+createColumn([X|Y],COLS):- 	
+							createColumn(X,0),
+							N is COLS-1,
+							createColumn(Y, N).
+							
 % Start the game
 start:- initBoard(Board), printBoard(Board).
