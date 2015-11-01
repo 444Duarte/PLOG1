349d398e6804da8d6e86f84bbe246121f15bfbdb
João Silva <joaosilva@revtut.net>
2015-11-01 21:26:02 +0000

    Added start function to received parameters

--- game.pl
+++ game.pl
@@ -1,23 +1,25 @@
-% Initialize the board
-initBoard([
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [1|2], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ],
-[ [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0], [0|0] ]
-]
-).
+% Create the board
+createBoard(BOARD, COLUMNS, ROWS):- 
+									createRow(BOARD, COLUMNS, ROWS).
+									
+createRow([X|[]], COLS, 1) :- 
+                            createColumn(X,COLS).
+createRow(X, COLS, 0):- 
+                            createColumn(X,COLS).						
+createRow( [X|Y],COLS,ROWS):- 
+							createRow(X, COLS, 0),
+							N is ROWS-1,
+							createRow(Y, COLS, N).
 
+createColumn([[0|0]] ,1).
+createColumn([0|0], 0).						
+createColumn([X|Y],COLS):- 	
+							createColumn(X,0),
+							N is COLS-1,
+							createColumn(Y, N).
+	
 % Print triangles
-printTriangle([0|_]) :- write('|   ').
+printTriangle([0|_]) :-     write('|   ').
 printTriangle([1|1]) :- 	write('| '),
 							name(C,[9651]), 
 							write(C),
@@ -37,38 +39,24 @@ printTriangle([2|2]) :- 	write('| '),
 
 % Print utils
 printBoarder([]):- write('-\n').
-printBoarder([_| NEXT_COLUMNS]):- 		write('----'),
-										printBoarder(NEXT_COLUMNS).
+printBoarder([_| NEXT_COLUMNS]):-   write('----'),
+									printBoarder(NEXT_COLUMNS).
+
+% Print Row
 printRow([]):- write('|\n').
-printRow([TRIANGLE | NEXT_TRIANGLES]) :-
-										printTriangle(TRIANGLE),
-										printRow(NEXT_TRIANGLES).
+printRow([TRIANGLE | NEXT_TRIANGLES]) :-    printTriangle(TRIANGLE),
+										    printRow(NEXT_TRIANGLES).
 
 % Print the board
 printBoard([ROW|[]]) :-	printBoarder(ROW),
-							printRow(ROW),
-							printBoarder(ROW). % Last row
-printBoard([ROW | NEXT_ROWS]) :-printBoarder(ROW),
-								printRow(ROW),
-								printBoard(NEXT_ROWS).
+						printRow(ROW),
+						printBoarder(ROW). % Last row
+printBoard([ROW | NEXT_ROWS]) :-    printBoarder(ROW),
+								    printRow(ROW),
+								    printBoard(NEXT_ROWS).
 
-								
-createBoard(BOARD, COLUMNS, ROWS):- 
-									createRow(BOARD, COLUMNS, ROWS).
-									
-createRow([X|[]], COLS, 1) :- createColumn(X,COLS).
-createRow(X, COLS, 0):- createColumn(X,COLS).						
-createRow( [X|Y],COLS,ROWS):- 
-							createRow(X, COLS, 0),
-							N is ROWS-1,
-							createRow(Y, COLS, N).
-
-createColumn([[0|0]] ,1).
-createColumn([0|0], 0).						
-createColumn([X|Y],COLS):- 	
-							createColumn(X,0),
-							N is COLS-1,
-							createColumn(Y, N).
-							
-% Start the game
-start:- initBoard(Board), printBoard(Board).
+% Start the game  
+start(COLS, ROWS) :-    createBoard(BOARD, COLS, ROWS),
+                        initBoard(BOARD),
+                        printBoard(BOARD).
+start:- start(10, 10).
