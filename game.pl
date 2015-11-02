aa950cf1b3c3c8a4f474783cbc248a7177d334c3
444Duarte <duartemr.pinto@gmail.com>
2015-11-02 05:28:58 +0000

    Menu inicial já feito

--- game.pl
+++ game.pl
@@ -60,3 +60,29 @@ start(COLS, ROWS) :-    createBoard(BOARD, COLS, ROWS),
                         initBoard(BOARD),
                         printBoard(BOARD).
 start:- start(10, 10).
+
+
+menu_option(1):-	write('Number of columns: '),
+					read(COLUMNS),
+					write('\nNumber of rows: '),
+					read(ROWS),
+					createBoard(BOARD, COLUMNS,ROWS),
+					printBoard(BOARD).
+
+
+menu_option(2):- 	write('/////////////\n'),
+					write('//  Rules  //\n'),
+					write('/////////////\n\n'),
+					write('Players alternate playing adjacent to the existing pattern.\nThe first player to form a four tile triangle with his color on all three tips (and either color in the center) wins.\n'),
+					write('\nInsert key any to go back\n'),	
+					read(A),start_game(A).
+
+start_game(_) :- 	write('///////////////////////\n'),
+					write('//      Spangles     //\n'),
+					write('//                   //\n'),
+					write('// Choose an option: //\n'),
+					write('//  1-Play           //\n'),
+					write('//  2-Rules          //\n'),
+					write('///////////////////////\n'),
+					read(OPTION), write('\n'),
+					menu_option(OPTION).
