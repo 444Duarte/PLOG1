8c001f63bb1bd7633de565e2a405c3d7ff19a6e3
João Silva <joaosilva@revtut.net>
2015-11-02 15:29:46 +0000

    Fixed board creation

--- game.pl
+++ game.pl
@@ -3,25 +3,19 @@
 :- include('validation.pl').
 
 % Create the board
-createBoard(BOARD, COLUMNS, ROWS):- 
-									createRow(BOARD, COLUMNS, ROWS).
+createBoard([], _, 0).
+createBoard([ROW | NEXT_ROWS], COLUMNS, ROWS):-     ROWS \= 0,
+                                                    createRow(ROW, COLUMNS),
+                                                    R1 is ROWS - 1,
+                                                    createBoard(NEXT_ROWS, COLUMNS, R1).
 									
-createRow([X|[]], COLS, 1) :- 
-                            createColumn(X,COLS).
-createRow(X, COLS, 0):- 
-                            createColumn(X,COLS).						
-createRow( [X|Y],COLS,ROWS):- 
-							createRow(X, COLS, 0),
-							N is ROWS-1,
-							createRow(Y, COLS, N).
+createRow([], 0).
+createRow([COLUMN | NEXT_COLUMNS], COLUMNS) :-  COLUMNS \= 0,
+                                                C1 is COLUMNS - 1,
+                                                createColumn(COLUMN),
+                                                createRow(NEXT_COLUMNS, C1).
+createColumn([0,0]).
 
-createColumn([[0|0]] ,1).
-createColumn([0|0], 0).						
-createColumn([X|Y],COLS):- 	
-							createColumn(X,0),
-							N is COLS-1,
-							createColumn(Y, N).
-	
 % Start the game  
 start:- start(10, 10).
 start(COLS, ROWS) :-    createBoard(BOARD, COLS, ROWS),
@@ -31,15 +25,16 @@ start(COLS, ROWS) :-    createBoard(BOARD, COLS, ROWS),
 nextPlayer(1, 2).
 nextPlayer(2, 1).
 
-% Insert triangles
-insertTriangleCol([[ | 1]| _], 0, 0, 1).
-insertTriangleCol([_ | NEXT_COLUMNS, COLUMN, 0, PLAYER) :-      COLUMN \= 0,
-                                                                C is COLUMN - 1,
-                                                                insertTriangleColumn(NEXT_COLUMNS, C, 0, PLAYER).
-insertTriangle([ROW | _], COLUMN, 0, PLAYER) :- insertTriangleCol(ROW, COLUMN, 0, PLAYER).  
-insertTriangle([_ | NEXT_ROWS], COLUMN, ROW, PLAYER) :- ROW \= 0,
-                                                        R is ROW - 1,
-                                                        insertTriangle(NEXT_ROWS, COLUMN, R, PLAYER).
+% Insert triangle - 1. column, 2. row, 3. result row, 4. value [triangle, player]
+insertTriangleCol(0, [_ | NCOLUMNS], [[T | P] | NCOLUMNS], [T | P]).
+insertTriangleCol(C, [COL | NCOLUMNS], [COL | RESULT_NCOLUMNS], VALUE) :-   C \= 0,
+                                                                            C1 is C - 1,
+                                                                            insertTriangleCol(C1, NCOLUMNS, RESULT_NCOLUMNS, VALUE).
+% Insert triangle - 1. column, 2. row, 3. board, 4. result board, 5. value [triangle, player]
+insertTriangle(C, 0, [ROW | NROW], [RESULT_ROW | NROW], VALUE) :- insertTriangleCol(C, ROW, RESULT_ROW, VALUE). 
+insertTriangle(C, R, [ROW | NROW], [ROW | RESULT_NROW], VALUE) :-   R \= 0,
+                                                                    R1 is R - 1,
+                                                                    insertTriangle(C, R1, NROW, RESULT_NROW, VALUE).
 
 % Game loop
 playGame(BOARD, PLAYER) :-  !,
