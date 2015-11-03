66dc19f538706f1bfce32ac47e0e37c1837a49b2
João Silva <joaosilva@revtut.net>
2015-11-03 10:20:15 +0000

    Removed board creation from game and moved her to utilities

--- game.pl
+++ game.pl
@@ -3,27 +3,6 @@
 :- include('validation.pl').
 :- include('utilities.pl').
 
-% Create the board
-createBoard(BOARD, COLUMNS, ROWS):- 
-									createRow(BOARD, COLUMNS, ROWS).
-									
-createRow([X|[]], COLS, 1) :- 
-                            createColumn(X,COLS).
-createRow(X, COLS, 0):- 
-                            createColumn(X,COLS).						
-createRow( [X|Y],COLS,ROWS):- 
-							createRow(X, COLS, 0),
-							N is ROWS-1,
-							createRow(Y, COLS, N).
-
-createColumn([[0|0]] ,1).
-createColumn([0|0], 0).						
-createColumn([X|Y],COLS):- 	
-							createColumn(X,0),
-							N is COLS-1,
-							createColumn(Y, N).
-
-
 % Start the game  
 start:- start(10, 10).
 start(COLS, ROWS) :-    createBoard(BOARD, COLS, ROWS),
