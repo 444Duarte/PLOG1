7308e4313871e03810b02e0f52c76ea3e1b8a5d8
João Silva <joaosilva@revtut.net>
2015-11-04 12:46:10 +0000

    Working on generating random coordinates for bot

--- bot.pl
+++ bot.pl
@@ -6,11 +6,14 @@
 getRandomCoord(BOARD, COLUMN, ROW) :-   getNumberRows(BOARD, NROWS),
                                         getNumberCols(BOARD, NCOLS),
                                         randomColumn(NCOLS, C1),
-                                        randomRow(BOARD, R1).
+                                        randomRow(BOARD, R1)
+                                        (validCoords(BOARD, C1, R1) ->
+                                                true;
+                                                !,
+                                                getRandomCoord(BOARD, COLUMN, ROW)).
 % Random column between 0 and number columns
 randomColumn(NCOLS, RESULT) :-  MAX_COL is NCOLS - 1,
                                 random_between(0, MAX_COL, RESULT).
 % Random row between 0 and number rows
-randomColumn(NCOLS, RESULT) :-  MAX_COL is NCOLS - 1,
 randomRow(NROWS, RESULT) :- MAX_ROW is NROWS - 1,
                             random_between(0, MAX_ROW, RESULT).
