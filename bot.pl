0c83443e5656efe36967a4b03d20f79f08c0eecf
444Duarte <duartemr.pinto@gmail.com>
2015-11-06 13:25:46 +0100

    Bot logic initiated and random play implemented

--- bot.pl
+++ bot.pl
@@ -16,3 +16,30 @@ randomColumn(NCOLS, RESULT) :-  MAX_COL is NCOLS - 1,
 % Random row between 0 and number rows
 randomRow(NROWS, RESULT) :- MAX_ROW is NROWS - 1,
                             random(0, MAX_ROW, RESULT).
+
+
+%Make a random move in a valid position of the board
+
+randomMove(BOARD, PLAYER):- getRandomCoord(BOARD, X, Y),
+                            calcTriangleType(BOARD, X, Y, TYPE),
+                            insertTriangle(X, Y, BOARD, RESULT, [TYPE | PLAYER]).
+
+
+%Print error message when the bot can´t make a play
+botErrorMessage:- write('BOT ERROR: Can't make a valid play!).
+
+%Try to finish the game by placing the final triangle tile
+finishGame(BOARD, PLAYER):- false.
+
+%Try to negate Enemy finishing move by placing a tile where the enemy player would place his.
+negateEnemyMove(BOARD, PLAYER):- false.
+
+
+% Make a play as a bot 1.board, 2. Bot's player number, 3.dificulty
+
+botTurn(BOARD, PLAYER, 1):- finishGame(BOARD, PLAYER).
+botTurn(BOARD, PLAYER, 1):- negateEnemyMove(BOARD, PLAYER).
+botTurn(BOARD, PLAYER, 1):- randomMove(BOARD, PLAYER).
+botTurn(BOARD, PLAYER, 1):- botErrorMessage.
+
+
