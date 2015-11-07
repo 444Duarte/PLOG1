0a499f0af9bfb95950861d098525533a8db7ca5a
João Silva <joaosilva@revtut.net>
2015-11-07 16:54:49 +0000

    Added bot vs bot mode. Updated menus

--- menus.pl
+++ menus.pl
@@ -6,7 +6,10 @@ menu_option('1'):-  readBoardSize(COLUMNS, ROWS),
 menu_option('2'):-  readBoardSize(COLUMNS, ROWS),
                     startPvB(COLUMNS, ROWS).
 
-menu_option('3'):-  write('/////////////\n'),
+menu_option('3'):-  readBoardSize(COLUMNS, ROWS),
+                    startBvB(COLUMNS, ROWS).
+
+menu_option('4'):-  write('/////////////\n'),
     				write('//  Rules  //\n'),
     				write('/////////////\n\n'),
     				write('Players alternate playing adjacent to the existing pattern.\nThe first player to form a four tile triangle with his color on all three tips (and either color in the center) wins.\n'),
@@ -17,6 +20,7 @@ menu_option('3'):-  write('/////////////\n'),
 menu_option(OPTION):-   OPTION \= '1',
                         OPTION \= '2',
                         OPTION \= '3',
+                        OPTION \= '4',
                         spangles.
 
 
@@ -26,7 +30,8 @@ spangles :- write('//////////////////////////\n'),
     		write('// Choose an option:    //\n'),
     		write('//  1-Player vs Player  //\n'),
     		write('//  2-Player vs Bot     //\n'),
-    		write('//  3-Rules             //\n'),
+    		write('//  3-Bot vs Bot        //\n'),
+    		write('//  4-Rules             //\n'),
     		write('//                      //\n'),
             write('//////////////////////////\n'),
             prompt(_, 'Option: '),
